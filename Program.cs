41dac8069cfb58dafe631e567226cf76c740136d
kevin <kachhadiyakevin03@gmail.com>
2025-02-09 16:23:36 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Moksha_App.Areas.Identity.Data;
@@ -18,12 +17,6 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
                 options.LogoutPath = "/Auth/Logout";
                 options.AccessDeniedPath = "/Auth/AccessDenied";
             });
-builder.Services.AddDataProtection()
-    .PersistKeysToFileSystem(new DirectoryInfo("/app-keys")) // Prevents /root/.aspnet issue
-    .SetApplicationName("MyApp") // Helps in multi-instance deployments
-    .DisableAutomaticKeyGeneration(); // Prevents unencrypted key warning
-
-
 builder.Services.AddControllersWithViews(options =>
 {
     options.Filters.Add(new GlobalTokenAuthorizationFilter(
@@ -46,7 +39,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-
+pu
 app.UseRouting();
 app.UseStaticFiles();
 
